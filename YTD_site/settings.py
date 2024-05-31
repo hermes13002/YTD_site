6313cdb352ef908e195560a853dce2f2989b6302
Soares Ayoigbala <soaresayoigbala@gmail.com>
2024-05-31 19:08:11 +0100

    settings.py static config

--- YTD_site/settings.py
+++ YTD_site/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
